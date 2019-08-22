1daffd228f68114cfabae7bdbed52c8eb634c6d6
Deep Bratt Mondal <deepbratt@gmail.com>
2019-08-22 17:24:34 +0530

    final learning complete

--- express_pack/public/templating.js
+++ express_pack/public/templating.js
@@ -1,13 +1,21 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 app.set('view engine', 'ejs');
+app.use('/assets', express.static('assets'));
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.get('/', function(req,res){
-  res.sendFile(__dirname + '/index.html');
+  res.render('index');
 });
 
 app.get('/contact', function(req,res){
-  res.send('this is the contact page');
+  res.render('contact',{ qs : req.query});
+});
+
+app.post('/contact', urlencodedParser, function(req,res){
+  console.log(req.body);
+  res.render('contact-success',{ data : req.body});
 });
 
 app.get('/profile/:id', function(req,res){
